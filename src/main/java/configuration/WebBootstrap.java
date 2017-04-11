3696a199766ce7ed50725e8b208c472751402cbf
Sasha <oleksandra_liubchenko@epam.com>
2017-04-11 21:04:03 +0300

    websockets

--- src/main/java/configuration/WebBootstrap.java
+++ src/main/java/configuration/WebBootstrap.java
@@ -6,6 +6,7 @@ import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+import web.websocket.WebSocketHandler;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -20,11 +21,11 @@ public class WebBootstrap implements WebApplicationInitializer {
 
         AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
         rootContext.register(BaseRootConfiguration.class);
-        rootContext.register(WebConfiguration.class);
         container.addListener(new ContextLoaderListener(rootContext));
 
         AnnotationConfigWebApplicationContext servletContext = new AnnotationConfigWebApplicationContext();
         servletContext.register(WebConfiguration.class);
+        servletContext.register(WebSocketConfiguration.class);
 
         ServletRegistration.Dynamic dispatcher = container.addServlet(
                 "springDispatcher",
